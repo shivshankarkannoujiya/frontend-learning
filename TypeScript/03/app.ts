d9e0f34b68f1186184d1a03d05aa7b5f503562eb
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-12-16 17:57:18 +0530

    add TypeScript examples for function types, optional parameters, default parameters, rest parameters, and overloads

---
+++ TypeScript/03/app.ts
@@ -0,0 +1,53 @@
+/**
+ * Functions
+ * Function Types
+ * Optional and default parameters
+ * Rest parameters
+ * Overloads 
+ */
+
+// function returnString(): string{
+//     return `returning Strings`
+// }
+
+// function returnNoting(): void{
+//     console.log(`Returning Nothing`);
+// }
+
+// function Abc(name: string, cb: (value: string) => void) {
+//     cb("Hello")
+//  }
+// Abc("Abhi", (value: string) => {
+//     console.log(value)
+// })
+
+
+// TODO: Optional parameter
+// function abcd(name: string, age: number, gender?: string) { }
+// abcd("harsh", 22, "male");
+// abcd("John", 23);
+
+// TODO: default parameter
+// function display(name: string, age: number, gender: string = "not to be disclosed") {
+//     console.table([name, age, gender])
+// }
+
+// display("John", 23, "Male")
+// display("Abhay", 22);
+
+
+// TODO: Rest Parameter
+
+// function sum(...args: number[]) {
+//     console.log(args)
+// }
+// sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+// function boys(...args: string[]) {
+//     console.log(args)
+// }
+
+// boys("Raj", "John", "Jockey");
+
+// TODO: function Overloading
+
